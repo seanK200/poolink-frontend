0ad2a580079290eb5e6db50b5bb3fd834399aa87
Yunsun Jung <yunsun992510@gmail.com>
2022-03-23 00:38:53 +0900

    modify Logo code

--- src/components/assets/Logo.js
+++ src/components/assets/Logo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function Logo({ ...rest }) {
   const navigate = useNavigate();
